5c55928279385d5f65b9b13a3d9976e4cac112c6
spullanoor <spullanoor@umass.edu>
2021-01-17 10:03:54 +0530

    starter code, yet to modify

---
+++ commands.py
@@ -0,0 +1,42 @@
+import time
+import random
+
+print('Hello! My name is ALECC. It is short for Aggies Love Existential Crisis Chatbots. '
+      'I might just be a program but I am a very loved program (... created to remind other people that they are also loved)')
+time.sleep(2)
+name = input("What is your name? ")
+print('Nice to meet you, ' +name)
+feeling = input("Are you doing alright?")
+time.sleep(2)
+if (negative response):
+       print(
+              'I’m here for you if you need to talk. Here is a link to a bunch of UC Davis mental health support resources:\n'
+              '	https://eachaggiematters.ucdavis.edu/campus-resources \n'
+              'Also these are the numbers to some hotlines: \n'
+              'National Suicide Prevention Lifeline: 800-273-8255 \n'
+              'Suicide Prevention of Yolo County, Davis: (530) 756-5000 \n'
+              'It sounds like you need some self-care. I can give you examples of self care if you like :)')
+
+want = input('Would you like some examples?')
+time.sleep(2)
+if "yes" or "yeah" or "nice" or "good" or "great" or "okay" or "ok" in want:
+print ('Listen to an interesting podcast or read your favorite book \n'
+       'Journal your feelings \n'
+       'Create some art \n'
+       'Play an online game or watch a show with a pal \n'
+       'This is my favorite online mini game platform https://backyard.co/ you can invite pals to play with you! \n'
+       'Eat something yummy \n'
+       'Go for a walk \n'
+       'Step outside and look at the sky \n'
+       'Do deep breathing exercises \n'
+       )
+okay = input('Do any of these sound nice to do right now?')
+if "yes" or "yeah" or "great" or "nice" in okay:
+       print('Okay! Let me know if you want to chat afterwards!')
+
+elif "no" or "Im okay" or "nah":
+       print('Thats alright! Wanna just chat?')
+
+
+
+
