f24147790ba3adf6fd2a45b930aff35afc3e026a
spullanoor <spullanoor@umass.edu>
2021-01-17 12:27:55 +0530

    updated without the chat section, incomplete

--- commands.py
+++ commands.py
@@ -9,7 +9,10 @@ time.sleep(3)
 
 #input the name
 name = input("What is your name? ")
+time.sleep(3)
+
 print('Nice to meet you, ' +name)
+time.sleep(3)
 
 feeling = input("Are you doing alright?")
 time.sleep(3)
@@ -38,13 +41,18 @@ print ('Listen to an interesting podcast or read your favorite book \n'
        'Step outside and look at the sky \n'
        'Do deep breathing exercises \n'
        )
+
 okay = input('Do any of these sound nice to do right now?')
+time.sleep(3)
+
 if "yes" or "yeah" or "great" or "nice" in okay:
        print('Okay! Let me know if you want to chat afterwards!')
        #end statement?
+
 elif "no" or "Im okay" or "nah" in okay:
        print('Thats alright! )'
        chat = input ('Would you like to chat ? ')
+
        if "yes" or "yeah" in chat:
        #chat
        elif "no"or "Im okay" or "nah" in chat:
@@ -53,12 +61,17 @@ elif "no" or "Im okay" or "nah" in okay:
 #from tweet-training
 if (positive response):
        print('this is so wonderful to hear!)
+
 student = input('Are you a student?')
+time.sleep(3)
+
 if "yes" or "yeah" in student:
        stress = input('Have your classes been stressing you at all?' )
        if "yes" or "yeah" in stress:
               print('I am always here if you need to talk!')
+                time.sleep(3)
               activites = input('I can give you examples of self-care activites if you would like')
+                time.sleep(3)
               if "yes" or "yeah" or "nice" or"good" or "great" or "ok" or "okay" in activities:
                      print ('Here you go : \n'
                             'Listen to an interesting podcast or read your favorite book \n'
@@ -73,11 +86,14 @@ if "yes" or "yeah" in student:
                             'And here is a link to a bunch of UC Davis mental health support resources '
                             'if you ever need them \n'
                             'https://eachaggiematters.ucdavis.edu/campus-resources'
+
               elif "no" or "nah" or "Im okay" in activites:
                      #chat
+
        elif "no" or "good" or "nah" in stress:
               print ('I seriously love hearing that!')
               selfcare = input('I can offer you some ideas of self care activities you can use whenever')
+                time.sleep(3)
               if "yes" or "yeah" or "nice" or"good" or "great" or "ok" or "okay" in selfcare:
                      print('Listen to an interesting podcast or read your favorite book \n'
                            'Journal your feelings \n'
@@ -89,11 +105,15 @@ if "yes" or "yeah" in student:
                            'Step outside and look at the sky \n'
                            'Do deep breathing exercises \n'
                            )
+
               elif "Im okay" or "nah" in selfcare:
                   #chat
+
 elif "not a student" or "no" or "nah" in student:
        print('Well I hope you are doing well in your life journey!')
        future = input('Can I offer you some examples of self-care you can do in the future?')
+       time.sleep(3)
+
        if "yes" or "yeah" or "nice" or "good" or "great" or "ok" or "okay" in future:
               print('Listen to an interesting podcast or read your favorite book \n'
                     'Journal your feelings \n'
@@ -105,5 +125,6 @@ elif "not a student" or "no" or "nah" in student:
                     'Step outside and look at the sky \n'
                     'Do deep breathing exercises \n'
                     )
+
        elif "Im okay" or "nah" in future:
        #chat
